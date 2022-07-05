41d42d8a513bedf609eafe9a031a5ea3c45b7667
oxedions <oxedions@gmail.com>
2022-07-05 15:33:07 +0200

    Dev - Add bond

--- networkd.py
+++ networkd.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-#from __future__ import absolute_import, division, print_function
-
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.common.text.converters import to_text
 import re
 import os
-
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.common.text.converters import to_text
 
 class NetworkdModuleError(Exception):
     pass
 
-class Networkd(object):
+def same_list_file(list1, filepath):
+    same = True
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            filepath_lines = f.readlines()
+        if len(list1) != len(filepath_lines):
+            same = False
+        else:
+            for i in range(0,len(list1),1):
+                if list1[i] != filepath_lines[i].replace("\n", ""):
+                    same = False
+    else:
+        same = False
+    return same
 
-    platform = 'Generic'
-    distribution = None
+def write_list_to_file(list1, filepath):
+    os.makedirs(filepath.split('/')[:-1], mode = 0o755, exist_ok=True)    
+    f = open(filepath, "w")
+    for it in list1:
+        f.write(it + "\n")
+    f.close()
+
+class Networkd(object):
 
     def __init__(self, module):
         self.module = module
         self.conn_name = module.params['conn_name']
+        self.master = module.params['master']
         self.state = module.params['state']
         self.ifname = module.params['ifname']
         self.type = module.params['type']
@@ -28,8 +44,9 @@ class Networkd(object):
         self.routes4 = module.params['routes4']
         self.dns4 = module.params['dns4']
         self.method4 = module.params['method4']
+        self.mode = module.params['mode']
 
-    def generate_networks(self):
+    def generate_network(self):
         network = []
 
         # MATCH
@@ -44,6 +61,9 @@ class Networkd(object):
         if self.dns4 is not None:
             for dns4 in self.dns4:
                 network.append("DNS=" + dns4)
+        if self.type == "bond-slave":
+            if self.master is not None:
+                network.append("Bond=" + self.master)
 
         # ADDRESS
         if self.method4 == "manual":
@@ -65,40 +85,48 @@ class Networkd(object):
 
         return network
 
+    def generate_netdev(self):
+        netdev = []
+
+        # NETDEV
+        netdev.append("[NetDev]")
+        netdev.append("Name=" + self.conn_name)
+
+        if self.type == "bond":
+            netdev.append("Kind=bond")
+
+        # BOND
+        if self.type == "bond":
+            netdev.append("[Bond]")
+            if self.mode is not None:
+                netdev.append("Mode=" + self.mode)
+            else:
+                netdev.append("Mode=802.3ad")
+
+
 def main():
     # Parsing argument file
     module = AnsibleModule(
         argument_spec=dict(
             state=dict(type='str', required=True, choices=['absent', 'present']),
             conn_name=dict(type='str', required=True),
+            master=dict(type='str'),
             ifname=dict(type='str'),
             type=dict(type='str',
                       choices=[
                           'bond',
                           'bond-slave',
-                          'bridge',
-                          'bridge-slave',
-                          'dummy',
                           'ethernet',
-                          'generic',
-                          'gre',
                           'infiniband',
-                          'ipip',
-                          'sit',
-                          'team',
-                          'team-slave',
                           'vlan',
-                          'vxlan',
-                          'wifi',
-                          'gsm',
-                          'wireguard',
-                          'vpn',
                       ]),
             ip4=dict(type='list', elements='str'),
             gw4=dict(type='str'),
             routes4=dict(type='list', elements='str'),
             dns4=dict(type='list', elements='str'),
             method4=dict(type='str', choices=['auto', 'link-local', 'manual', 'shared', 'disabled']),
+            mode=dict(type='str', default='balance-rr',
+                      choices=['802.3ad', 'active-backup', 'balance-alb', 'balance-rr', 'balance-tlb', 'balance-xor', 'broadcast']),
         ),
         mutually_exclusive=[],
         required_if=[],
@@ -110,7 +138,7 @@ def main():
 
     (rc, out, err) = (None, '', '')
     result = {'conn_name': networkd.conn_name, 'state': networkd.state}
-    changed = 0
+    changed = False
 
 
     # check for issues
@@ -122,33 +150,35 @@ def main():
             # Simply check no files are associated with this conn_name
             print("Absent")
         elif networkd.state == 'present':
-            # Generate Network file
-            network = networkd.generate_networks()
-            # Read current Network file if exist and compare if changes
-            network_file = "/etc/systemd/network/" + networkd.conn_name +".network"
-            if os.path.exists(network_file):
-                with open("/etc/systemd/network/" + networkd.conn_name +".network") as f:
-                    network_lines = f.readlines()
-                if len(network) != len(network_lines):
-                    changed = 1
-                else:
-                    for i in range(0,len(network),1):
-                        if network[i] != network_lines[i].replace("\n", ""):
-                            changed = 1
-            else:
-                changed = 1
 
-            # Write configuration if changes detected
-            if changed == 1:
-                f = open(network_file, "w")
-                for it in network:
-                    f.write(it + "\n")
-                f.close()
+            if networkd.type in ['ethernet','infiniband','bond-slave']:
+
+                # Generate Network file
+                network = networkd.generate_network()
+                # Read current Network file if exist and compare if changes
+                network_file = "/etc/systemd/network/" + networkd.conn_name +".network"
+                # Check if content is the same
+                changed = not same_list_file(network,network_file)
+                # Write configuration if changes detected
+                if changed:
+                    write_list_to_file(network, network_file)
+
+            elif networkd.type in ['bond']:
+
+                # Generate Netdev file
+                netdev = networkd.generate_netdev()
+                # Read current Network file if exist and compare if changes
+                netdev_file = "/etc/systemd/network/" + networkd.conn_name +".netdev"
+                # Check if content is the same
+                changed = not same_list_file(netdev,netdev_file)
+                # Write configuration if changes detected
+                if changed:
+                    write_list_to_file(netdev, netdev_file)
 
             # Post actions
             if changed == 1:
-                stdout, stderr = subprocess.Popen("networkctl reload", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
-                stdout, stderr = subprocess.Popen("networkctl reconfigure " + self.conn_name, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
+                stdout, stderr = os.subprocess.Popen("networkctl reload", stdout=os.subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
+                stdout, stderr = os.subprocess.Popen("networkctl reconfigure " + networkd.conn_name, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
 
     except NetworkdModuleError as e:
         module.fail_json(name=networkd.conn_name, msg=str(e))
